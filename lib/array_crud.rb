1799d77da96a49925b75f737f4d47ae6669d4f2a
Devone Austin <devone.austin@gmail.com>
2019-11-21 20:47:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_family = ["Phil", "Devone", "Felix","Soma"]
-  my_family.push("arrays!")
+  array = ["wow", "I", "am", "really", "learning"]
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
